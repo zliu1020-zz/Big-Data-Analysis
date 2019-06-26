cde9ce6629ea3545ea4deff6183c336cb5889c91
Ziyan Liu <z363liu@uwaterloo.ca>
2019-06-26 17:16:27 -0400

    task1 scala is not working

--- Task1.scala
+++ Task1.scala
@@ -9,7 +9,28 @@ object Task1 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
+    val output = textFile.map(x => {
+        var values = x.split(",", -1)
+        var movieName = values(0)
+        var max = -1
+        var result = new StringBuilder
+
+        for(i <- 1 until values.length){
+            var rating = 0;
+            if(values(i) != ""){
+                rating = values(i).toInt
+            }
+
+            if(rating == max){
+                result ++= i.toString
+            }else if(rating > max){
+                max = rating
+                result.clear()
+                result ++= i.toString
+            }
+            movieName + "," + result.toString
+        }
+    })
     
     output.saveAsTextFile(args(1))
   }
