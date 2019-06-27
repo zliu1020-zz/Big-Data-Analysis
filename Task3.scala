42bf58b00c5a444b6d6aa1d138c78fb85003aa76
Zuowei Fu <z32fu@uwaterloo.ca>
2019-06-27 19:05:09 -0400

    tp is hero

--- Task3.scala
+++ Task3.scala
@@ -2,14 +2,22 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
 object Task3 {
+
+  def map(line: String): Array[(Int, Int)] = {
+    line.split(",", -1)
+    .zipWithIndex
+    .drop(1)
+    .filter(_._1 != "")
+    .map(pair => (pair._2, 1))
+  }
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 3")
     val sc = new SparkContext(conf)
-
     val textFile = sc.textFile(args(0))
-
-    // modify this code
-    val output = textFile.map(x => x);
+    val output = textFile.flatMap(line => map(line))
+    .groupByKey()
+    .map(v => (v._1,v._2.size));
     
     output.saveAsTextFile(args(1))
   }
