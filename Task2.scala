36552254e26af9b43dba72b91f984d27b52d4497
fuyono <tppp951618@gmail.com>
2019-06-26 20:24:50 -0400

    scala is fun :)

--- Task2.scala
+++ Task2.scala
@@ -2,6 +2,19 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
 object Task2 {
+  def countPerLine(line: String) : (Int, Int) = {
+  	var values = line.split(",", -1)
+  	var sum = 0
+  	for (i <- 1 until values.length) {
+  		if(values(i) != "") {
+  			sum = sum + 1
+  		}
+  	}
+  	// the key is used for aggregation in reduceByKey function 
+  	(0, sum)
+  }
+
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 2")
     val sc = new SparkContext(conf)
@@ -9,7 +22,14 @@ object Task2 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
+    val output = textFile
+    	.map(line => countPerLine(line))
+    	// the output is (0, 13) after reduceByKey
+    	// limit number of partition to be 1
+    	.reduceByKey(_+_, 1)
+    	// output the second paramter in the tuple (0, 13)
+    	.map(_._2)
+
     
     output.saveAsTextFile(args(1))
   }
