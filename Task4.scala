646d7b9908c682e7421b45930817de0889a54e28
fuyono <tppp951618@gmail.com>
2019-07-01 00:42:15 -0400

    🚀

--- Task4.scala
+++ Task4.scala
@@ -2,24 +2,57 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 object Task4 {
 
-implicit class Crossable[X](xs: Traversable[X]) {
-  def cross[Y](ys: Traversable[Y]) = for { x <- xs; y <- ys } yield (x, y)
-}
+  var moviesCombination: Array[(String, String)] = Array()
 
-  def processPerLine(line: String) : Array[(Int, String, Int)] = {
-  	var values = line.split(",").zipWithIndex
-  	// return format is (userIndex, movieName, rating)
-    var result : Array[(Int, String, Int)] = Array()
-    var movieName = values(0)._1
+  def calculateSimilarity(currentLine: String, textFile: Array[String]) : Array[String] = {
+    // need -1 in split function to split empty ratings
+  	var currentLineSplit = currentLine.split(",", -1)
+    var similarity = 0;
+    var result: Array[String] = Array()
 
-  	for (i <- 1 until values.length) {
-  		// filter out the blank ratings 
-  		if(values(i)._1 != "") {
-  			result = result :+ (values(i)._2, movieName, values(i)._1.toInt)
-  		}
-  	}
-  	result
-  } 
+    // loop the whole textFile
+    for (i <- 0 until textFile.length) {
+      // split each line
+      var lineSplit = textFile(i).split(",", -1)
+
+      var smallerMoive = currentLineSplit(0)
+      var biggerMovie = lineSplit(0)
+
+      // check and compare movies name
+      // movies names should be in ascending order
+      if (smallerMoive != "" && biggerMovie != "") {
+        if (smallerMoive > biggerMovie) {
+          smallerMoive = lineSplit(0)
+          biggerMovie = currentLineSplit(0)
+        }
+
+        var combine = (smallerMoive, biggerMovie)
+
+        // check if the movie names equal to avoid self-compare
+        // does not redo the similarity calculation if it is already done
+        if ( (smallerMoive != biggerMovie) && !(moviesCombination.contains(combine)) ) {
+          // loop from the first ratings and calculate similarity
+          for (j <- 1 until lineSplit.length) {
+            // does not count blank ratings
+            if ((currentLineSplit(j) != "") && (lineSplit(j) != "")) {
+              // increase the similarity if ratings are the same
+              if (currentLineSplit(j) == lineSplit(j)) {
+                similarity = similarity + 1
+              }
+            }
+          }
+          // stores the movie names combination
+          moviesCombination = moviesCombination :+ combine
+          // stores the result
+          result = result :+ smallerMoive + "," + biggerMovie + "," + similarity
+          // reset 
+          similarity = 0
+        }
+      }
+    }
+    result
+  }
+
 
 
 
@@ -28,29 +61,13 @@ implicit class Crossable[X](xs: Traversable[X]) {
     val sc = new SparkContext(conf)
 
     val textFile = sc.textFile(args(0))
+    // broadcast input textFile
+    val broadcastedTextFile = sc.broadcast(textFile.collect).value
 
-    // use flatMap to output the result
-    // presist is used for optimazation
-    val movieRatings = textFile.flatMap(line => processPerLine(line)).persist()
+    val moviePairRatings = broadcastedTextFile.flatMap(line => calculateSimilarity(line, broadcastedTextFile))
 
-    // broadcast is used for map-side join
-    // val broadcastVar = sc.broadcast(movieRatings.collect).value
-    // movieRatings.saveAsTextFile(args(1))
-    // val similarityData = broadcastVar.cartesian(broadcastVar).saveAsTextFile(args(1))
-
-    val moviePairData = movieRatings.cartesian(movieRatings)
-    								// only keep records which are in ascending lexicographic order
-    								.filter{ case ((userA, movieA, ratingA), (userB, movieB, ratingB)) => movieA < movieB && userA == userB }
-    								// calculate similarity, format (movieA, movieB, similarity)
-                    				.map{ case ((userA, movieA, ratingA), (userB, movieB, ratingB)) => ( movieA + "," + movieB, 
-                          				( if( userA == userB && ratingA == ratingB) 1 else 0 ))}
-                    				// aggreate records by key (movieA, movieB)
-                    				// a, b are similarityData
-                    				// 1 stands for number of partition 
-                    				.reduceByKey((a,b) => (a + b), 1)
-                    				// format output
-                    				.map{case (movies, similarityData) => movies + "," + similarityData}
-    								.saveAsTextFile(args(1))
-    // sc.parallelize(moviePairData).saveAsTextFile(args(1))
+    // 1 means the partition number is 1
+    sc.parallelize(moviePairRatings, 1).saveAsTextFile(args(1))
+    
   }
 }
