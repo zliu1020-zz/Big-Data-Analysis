d4c10986cfc7be8141c05fe79a5cbd4cd2ab0af2
fuyono <tppp951618@gmail.com>
2019-07-02 08:54:39 -0400

    qkbnb

--- Task4.scala
+++ Task4.scala
@@ -1,38 +1,53 @@
 import org.apache.spark.{SparkContext, SparkConf}
+import scala.collection.mutable.HashMap
 
 object Task4 {
+  var moviesCombination: HashMap[String, Boolean] = HashMap.empty[String, Boolean]
 
-  def calculateSimilarity(currentLine: String, textFile: Array[String]) : Array[String] = {
+  def calculateSimilarity(currentLine: String, textFile: Array[String], currengLineIndex: Int) : Array[String] = {
     // need -1 in split function to split empty ratings
     var currentLineSplit = currentLine.split(",", -1)
     var similarity = 0;
     var result: Array[String] = Array()
 
-    // loop the whole textFile
-    for (i <- 0 until textFile.length) {
+    // loop the whole textFile from currengLineIndex
+    for (i <- currengLineIndex until textFile.length) {
       // split each line
       var lineSplit = textFile(i).split(",", -1)
 
       var smallerMoive = currentLineSplit(0)
       var biggerMovie = lineSplit(0)
 
-      // only calculate the similarity if in ascending order
-      if ( smallerMoive < biggerMovie) {
-        // loop from the first ratings and calculate similarity
-        for (j <- 1 until lineSplit.length) {
-          // does not count blank ratings
-          if ((currentLineSplit(j) != "") && (lineSplit(j) != "")) {
-            // increase the similarity if ratings are the same
-            if (currentLineSplit(j) == lineSplit(j)) {
-              similarity = similarity + 1
-            }
-          }
+      // check and compare movies name
+      // movies names should be in ascending order
+      if (smallerMoive != "" && biggerMovie != "") {
+        if (smallerMoive > biggerMovie) {
+          smallerMoive = lineSplit(0)
+          biggerMovie = currentLineSplit(0)
         }
 
-        // stores the result
-        result = result :+ smallerMoive + "," + biggerMovie + "," + similarity
-        // reset 
-        similarity = 0
+        var combine = smallerMoive + "," + biggerMovie
+
+        // check if the movie names equal to avoid self-compare
+        // does not redo the similarity calculation if already done
+        if ( (smallerMoive != biggerMovie) && !(moviesCombination.contains(combine)) ) {
+          // loop from the first ratings and calculate similarity
+          for (j <- 1 until lineSplit.length) {
+            // does not count blank ratings
+            if ((currentLineSplit(j) != "") && (lineSplit(j) != "")) {
+              // increase the similarity if ratings are the same
+              if (currentLineSplit(j) == lineSplit(j)) {
+                similarity = similarity + 1
+              }
+            }
+          }
+          // stores the movie names combination
+          moviesCombination += (combine -> true)
+          // stores the result
+          result = result :+ combine + "," + similarity
+          // reset 
+          similarity = 0
+        }
       }
     }
     result
@@ -49,7 +64,8 @@ object Task4 {
     // broadcast input textFile
     val broadcastedTextFile = sc.broadcast(textFile.collect).value
 
-    val moviePairRatings = broadcastedTextFile.flatMap(line => calculateSimilarity(line, broadcastedTextFile))
+    // pass currentLineIndex into the function
+    val moviePairRatings = broadcastedTextFile.zipWithIndex.flatMap{case (line, index) => calculateSimilarity(line, broadcastedTextFile, index)}
 
     // 1 means the partition number is 1
     sc.parallelize(moviePairRatings, 1).saveAsTextFile(args(1))
